f54b3274dc40fbab0bc2044f83ef7b6c80cbf281
vero-sudo <elias.mikael@icloud.com>
2024-12-15 23:53:42 +0000

    Updated

--- index.js
+++ index.js
@@ -1,16 +1,12 @@
 const { Client, Events, GatewayIntentBits } = require('discord.js');
-const { token } = require('./config.json');
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 const fs = require('node:fs');
 const path = require('node:path');
 const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
-const { token } = require('./config.json');
 
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 client.commands = new Collection();
-
 
 client.once(Events.ClientReady, readyClient => {
 	console.log(`Ready! Logged in as ${readyClient.user.tag}`);
@@ -37,8 +33,6 @@ client.on(Events.InteractionCreate, async interaction => {
 		}
 	}
 });
-
-
 
 
 client.login(process.env.DISCORD_TOKEN);
