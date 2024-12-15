c96dffb3a8e3f26c0a93e94820ec98d7dbe0e634
vero-sudo <elias.mikael@icloud.com>
2024-12-15 15:59:55 +0000

    000

--- commands/utility/changedata.js
+++ commands/utility/changedata.js
@@ -4,9 +4,7 @@ module.exports = {
   data: new SlashCommandBuilder()
     .setName('request_data_change')
     .setDescription('Submit a request to change data'),
-	.addStringOption(option =>
-		
-	)
+
   async execute(interaction) {
     // Reply to the user
     await interaction.reply({ content: "Request submitted.", flags: MessageFlags.Ephemeral });
