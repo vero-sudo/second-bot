36a18f0cf3bc554d1e8c3ebb903aa1ff3b8b8060
vero-sudo <elias.mikael@icloud.com>
2024-12-15 11:47:53 +0000

    initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.once('ready', () => {
+  console.log('Bot is online!');
+});
+
+client.login(process.env.DISCORD_TOKEN);
