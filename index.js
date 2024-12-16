38c8f7ea440b75dbf9812c0aa6cecde0b5e91e23
vero-sudo <elias.mikael@icloud.com>
2024-12-16 00:29:30 +0000

    asd

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 const fs = require('node:fs');
 const path = require('node:path');
+const foldersPath = path.join(__dirname, 'commands');
+const commandFolders = fs.readdirSync(foldersPath);
 
 
 client.commands = new Collection();
@@ -10,6 +12,20 @@ client.commands = new Collection();
 client.once(Events.ClientReady, readyClient => {
 	console.log(`Ready! Logged in as ${readyClient.user.tag}`);
 });
+
+for (const folder of commandFolders) {
+	const commandsPath = path.join(foldersPath, folder);
+	const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+	for (const file of commandFiles) {
+	  const filePath = path.join(commandsPath, file);
+	  const command = require(filePath);
+	  if ('data' in command && 'execute' in command) {
+		client.commands.set(command.data.name, command);
+	  } else {
+		console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+	  }
+	}
+  }
 
 client.on(Events.InteractionCreate, async interaction => {
 	if (!interaction.isChatInputCommand()) return;
