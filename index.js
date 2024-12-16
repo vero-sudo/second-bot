c4af65aa86d617fa1e4fae7f4769bfec42b36163
vero-sudo <elias.mikael@icloud.com>
2024-12-16 15:00:46 +0000

    Updated

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ client.commands = new Collection();
 const foldersPath = path.join(__dirname, 'commands');
 const commandFolders = fs.readdirSync(foldersPath);
 
-const requestCommand = require('./commands/utility/request.js');
+// const requestCommand = require('./commands/utility/.request.js');
 
 for (const folder of commandFolders) {
     const commandsPath = path.join(foldersPath, folder);
