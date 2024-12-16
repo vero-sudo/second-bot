b823dbbe73b5a3c3ecd038e2ed83c6f8349f8cb3
vero-sudo <elias.mikael@icloud.com>
2024-12-16 15:06:04 +0000

    Updated

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
 const fs = require('node:fs');
 const path = require('node:path');
-const { Client, Collection, Events, GatewayIntentBits, MessageFlags } = require('discord.js');  // Added MessageFlags import
+const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
 
-const { DISCORD_TOKEN: token, CLIENT_ID: clientId, GUILD_ID: guildId } = process.env;
+const { DISCORD_TOKEN: token } = process.env;
+
 const client = new Client({
     intents: [
-      GatewayIntentBits.Guilds,
-      GatewayIntentBits.GuildMessages,
-      GatewayIntentBits.MessageContent,
-      GatewayIntentBits.GuildMembers
-    ]
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent,
+        GatewayIntentBits.GuildMembers,
+    ],
 });
 
 client.commands = new Collection();
 
+// Load commands
 const foldersPath = path.join(__dirname, 'commands');
 const commandFolders = fs.readdirSync(foldersPath);
-
-// const requestCommand = require('./commands/utility/.request.js');
 
 for (const folder of commandFolders) {
     const commandsPath = path.join(foldersPath, folder);
@@ -33,10 +33,11 @@ for (const folder of commandFolders) {
     }
 }
 
+// Handle interactions
 client.on(Events.InteractionCreate, async interaction => {
     if (!interaction.isChatInputCommand()) return;
 
-    const command = interaction.client.commands.get(interaction.commandName);
+    const command = client.commands.get(interaction.commandName);
 
     if (!command) {
         console.error(`No command matching ${interaction.commandName} was found.`);
@@ -44,40 +45,23 @@ client.on(Events.InteractionCreate, async interaction => {
     }
 
     try {
-        // Defer the reply if not already done
         if (!interaction.replied && !interaction.deferred) {
             await interaction.deferReply();
         }
-
-        // Execute the command
         await command.execute(interaction);
-
     } catch (error) {
         console.error(error);
 
-        // Only respond if interaction hasn't been acknowledged yet
-        if (!interaction.replied && !interaction.deferred) {
-            await interaction.reply({ content: 'There was an error while executing this command!', flags: MessageFlags.Ephemeral });
-        } else {
-            await interaction.followUp({ content: 'There was an error while executing this command!', flags: MessageFlags.Ephemeral });
+        if (!interaction.replied) {
+            await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
         }
     }
 });
 
-
-
+// Log bot ready event
 client.once(Events.ClientReady, readyClient => {
     console.log(`Ready! Logged in as ${readyClient.user.tag}`);
 });
 
-client.on('interactionCreate', async (interaction) => {
-    if (interaction.isCommand()) {
-        if (interaction.commandName === 'request') {
-            await requestCommand.execute(interaction);
-        }
-    } else if (interaction.isButton()) {
-        await requestCommand.buttonInteractionHandler(interaction);
-    }
-});
-
+// Login bot
 client.login(token);
