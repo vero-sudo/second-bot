740c6ea5c1a978b259c179932cf0f958eaa486cc
mikael37 <elias.mikael@icloud.com>
2024-12-19 21:42:10 +0000

    .

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const fs = require("node:fs");
 const path = require("node:path");
 const { Client, Collection, Events, GatewayIntentBits } = require("discord.js");
 const interactionHandler = require("./interactionHandler");
-
+process.noDeprecation = true;
 
 const { DISCORD_TOKEN: token } = process.env;
 
